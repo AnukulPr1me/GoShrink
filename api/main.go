bdce8cc6bf2d87fde9a5324de70ea4cde97c8d4c
anukulpr1me <anukul_chandra@outlook.com>
2024-09-19 13:02:19 +0530

    main setup

---
+++ api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import(
+	"fmt"
+	"log"
+	"os"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/joho/godotenv"
+)
+
+func setupRoutes(app *fiber.app){
+	app.get("/:url", routes.ResolveURL)
+	app.post("/api/v1", routes.ShortenURL)
+}
+
+func main(){
+	err:=godotenv.Load()
+	if err!=nil {
+        fmt.println(err)
+    }
+	app := fiber.New()
+	app.use(logger.New())
+	setupRoutes(app)
+	log.Fatal(app.Listen(os.Getenv("APP_PORT")))
+
+}
+
