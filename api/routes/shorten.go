c0faf9b39f9d1eff6f4dcd9addbacd76519d1f57
anukulpr1me <anukul_chandra@outlook.com>
2024-09-19 13:56:10 +0530

    helpers functions

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -17,3 +17,27 @@ type response struct{
 	XRateRemaining		int				`json:"rate_limit"`
 	XRateLimitRest		time.Duration	`json:"rate_limit_reset"`
 }
+
+func ShortenURL(c *fiber.ctx) error {
+	body:=new(request)
+	if err:=c.BodyParser(&body); err!=nil{
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+            "error": err.Error(),
+        })
+	}
+
+
+	if !govalidator.IsURL(body.URL){
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+            "error": "Invalid URL",
+        })
+	}
+
+	if !helpers.RemoveDomainError(body.URL){
+		return c.Status(fiber.statusServiceUnavailable).JSON(fiber.Map{
+            "error": "URL contains disallowed domain",
+        })
+	}
+
+	body.URL = helpers.EnforcHTTP(body.URL)
+}
