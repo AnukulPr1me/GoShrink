19ecc42db517853f8af48b66758e97b7b5fa5a62
anukulpr1me <anukul_chandra@outlook.com>
2024-09-23 16:00:07 +0530

    time expire

---
+++ api/routes/resolve.go
@@ -0,0 +1,31 @@
+package routes 
+
+import (
+	"github.com/anukulpr1me/GoShrink"
+	"github.com/gofiber/fiber/v2"
+    "github.com/gofiber/fiber/v2/middleware/logger"
+    "github.com/joho/godotenv"
+)
+
+func ResolveURL(c *fiber.Ctx) error{
+	url:= c.Params("url")
+	r::=database.CreateClient(0)
+	defer r.Close()
+	value, err := r.Get(database.Ctx, url)
+	if err == radis.Nil{
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+            "error": "URL not found",
+        })
+	}else if err != nil{
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+            "error": err.Error(),
+        })
+	}
+
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+
+	_=rInr.Incr(database.Ctx, "counter")
+
+	return c.Redirect(value, 301)
+}
--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,7 +1,9 @@
 Package routes
 
 import (
-
+	"time"
+	"github.com/anukulpr1me/GoShrink/database"
+	"os"
 )
 
 type request struct{
@@ -26,7 +28,22 @@ func ShortenURL(c *fiber.ctx) error {
         })
 	}
 
-
+	r2 := database.CreateClient(1)
+	defer r2.Close()
+	val,err := r2.Get(databaseCtx, c.IP()).Result()
+	if err == redis.Nil {
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*time.Second).Err()
+	}else{
+		r2.Get(database.Ctx, c.IP().Result())
+		ValInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			limit, _ := r2.TTL(database.Ctx, c.IP().Result())
+			return c.Status(fiber.statusServiceUnavailable).JSON(fiber.Map){
+				"error": "rate limit exceeded",
+				"rate_limit_reset": limit/limit.Nanosecond/limit.Minute,
+			}
+		}
+	}
 	if !govalidator.IsURL(body.URL){
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
             "error": "Invalid URL",
@@ -40,4 +57,29 @@ func ShortenURL(c *fiber.ctx) error {
 	}
 
 	body.URL = helpers.EnforcHTTP(body.URL)
+	var id string
+	if body.CustomShort == "" {
+		id = uuid.New().String()[:6]
+	}else{
+		id = body.CustomShort
+	}
+	r := database.CreateClient(0)
+	defer r.Close()
+	val, _ = r.Get(database.Ctx, id).Result()
+	if(val != ""){
+		return c.Status(fiber.StatusForBidden).JSON(fiber.Map{
+			"error": "URL custom short URL is alrady exists",
+		})
+	}
+	if body.Expiry == 0{
+		body.Expiry = 24
+	}
+
+	err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+            "error": "Unable to connect to server",
+        })
+	}
+	r2.Decr(database.Ctx, c.IP())
 }
